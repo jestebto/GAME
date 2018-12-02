5b15ea8d31b6d0611ac3bd0bea489e8b88ee1399
Lior Sinai <sinailior@gmail.com>
2018-12-02 12:56:25 +0100

    Added enemy data to HardCodedStorageManager

--- GAMEsource/HardCodedStorageManager.cpp
+++ GAMEsource/HardCodedStorageManager.cpp
@@ -2,7 +2,7 @@
 #include "HardCodedStorageManager.h"
 #include <string>
 
-static std::string defLevel ="0&0000-0110-0100-0000;1&001,1,2,3,;";
+static std::string defLevel ="0&0000-0110-0100-0000;1&001,1,2,3,;2&002,2,1,5,1,;";
 //"001/m&Default Map&0000-0110-0100-0000/0&0000-0110-0100-0000;"
 
 HardCodedStorageManager::HardCodedStorageManager()
