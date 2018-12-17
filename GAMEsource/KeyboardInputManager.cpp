ca9bb3dc30e92e4a1391a2f31c9d9f90fca78e24
jestebto <jestebto@gmail.com>
2018-12-17 16:27:03 +0100

    Fixed accumulated inputs, now it flushes them every time is read.

--- GAMEsource/KeyboardInputManager.cpp
+++ GAMEsource/KeyboardInputManager.cpp
@@ -21,6 +21,7 @@ UserInputType KeyboardInputManager::getInput()
 	// Based on the SDL2 toolkit.
 	// See https://wiki.libsdl.org/FrontPage
 	SDL_Event e;
+	UserInputType inputType = UserInputType::Nothing;
 	while (SDL_PollEvent(&e)) {
 		
 		// All events trigged by pushing a keyboard key down
@@ -29,28 +30,35 @@ UserInputType KeyboardInputManager::getInput()
 				case SDLK_a:
 				case SDLK_LEFT:
 					std::cout << ">>>Pressed LEFT!\n"; //<TO DO> comment out or remove (DEBUG)
-					return UserInputType::Left;
+					inputType = UserInputType::Left;
+					break;
 				case SDLK_d:
 				case SDLK_RIGHT:
 					std::cout << ">>>Pressed RIGHT!\n"; //<TO DO> comment out or remove (DEBUG)
-					return UserInputType::Right;
+					inputType = UserInputType::Right;
+					break;
 				case SDLK_w:
 				case SDLK_UP:
 					std::cout << ">>>Pressed UP!\n"; //<TO DO> comment out or remove (DEBUG)
-					return UserInputType::Up;
+					inputType = UserInputType::Up;
+					break;
 				case SDLK_s:
 				case SDLK_DOWN:
 					std::cout << ">>>Pressed DOWN!\n"; //<TO DO> comment out or remove (DEBUG)
-					return UserInputType::Down;
+					inputType = UserInputType::Down;
+					break;
 				case SDLK_k:
 				case SDLK_SPACE: //space bar //<TO DO JOSE> change with the right enum value
 					std::cout << ">>>Pressed HIT!\n";
-					return UserInputType::Hit;
+					inputType = UserInputType::Hit;
+					break;
 				case SDLK_ESCAPE:
 					std::cout << ">>>Pressed QUIT!\n"; //<TO DO> comment out or remove (DEBUG)
-					return UserInputType::Quit;
+					inputType =  UserInputType::Quit;
+					break;
 				default:
 					std::cout << "The key you pressed is not mapped. The event is: " << e.key.keysym.sym << "\n";
+					break;
 			}
 		}
 		else if (e.type == SDL_QUIT) { // Quit button in the window
@@ -58,5 +66,5 @@ UserInputType KeyboardInputManager::getInput()
 			return UserInputType::Quit;
 		}
 	}
-	return UserInputType::Nothing;
+	return inputType;
 }
