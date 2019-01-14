f83c370a20f4256a75bb840c8f7bb284bc553d14
jestebto <jestebto@gmail.com>
2019-01-14 12:05:17 +0100

    Weapon class is finally there

---
+++ GAMEsource/logic/Weapon.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "GameObject.h"
+
+class Weapon : public GameObject {
+public:
+	// input: ID,XPOS,YPOS,DAMAGE
+	Weapon(std::string, int, int, const int);
+	virtual ~Weapon() = default; // virtual destructor
+
+	int getDamage() const; // get the damage 
+
+private:
+	int damageValue;  /// damage added 
+};
+
