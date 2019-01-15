8751dcfe39978f33c88f3a6259bf4292418f1dcc
Lior Sinai <sinailior@gmail.com>
2019-01-15 21:26:05 +0100

    Change of name of the linux file

--- compile_GAME.sh
+++ compile_GAME.sh
@@ -1,1 +1,1 @@
-g++ -std=c++14  GAMEsource/*.cpp GAMEsource/*/*.cpp -Wall -Wextra -I GAMEsource/include -o GAME_linux -lSDL2 -I /opt/local/include -L /opt/local/lib./GAME_linux
+g++ -std=c++14  GAMEsource/*.cpp GAMEsource/*/*.cpp -Wall -Wextra -I GAMEsource/include -o GAMEsource_linux -lSDL2 -I /opt/local/include -L /opt/local/lib./GAMEsource_linux
